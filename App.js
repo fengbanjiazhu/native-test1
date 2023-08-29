3e591eb8c3b08cd06897a8740b859799caa90aa0
Jeffrey <562475805lanu@gmail.com>
2023-08-29 20:32:56 +1000

    practice styles & inputs

--- App.js
+++ App.js
@@ -1,20 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+import { Button, StyleSheet, Text, View, TextInput } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View>
+        <TextInput placeholder="test" style={styles.input}></TextInput>
+        <Button style={styles.button} title="ADD" />
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 50,
+  },
+  input: {
+    height: 45,
+    fontSize: 25,
+    borderWidth: 1,
+    padding: 10,
+  },
+  button: {
+    borderWidth: 1,
+    backgroundColor: "blue",
   },
 });
