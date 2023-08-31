bdfd99d5177a6274352a0c891c58fb8304920d37
Jeffrey <562475805lanu@gmail.com>
2023-08-31 21:48:55 +1000

    test TouchableHighlight & TouchableOpacity

--- components/ToDoItem.js
+++ components/ToDoItem.js
@@ -1,12 +1,22 @@
 import React from "react";
-import { Text, View, StyleSheet } from "react-native";
+import { Text, View, StyleSheet, TouchableOpacity, TouchableHighlight } from "react-native";
 
 function ToDoItem({ item, onDelete }) {
   return (
-    <View style={styles.list}>
-      <Text onPress={() => onDelete(item.uid)}>{item.value}</Text>
-    </View>
+    <TouchableHighlight underlayColor="#FF4D4F" onPress={() => onDelete(item.uid)}>
+      <View style={styles.list}>
+        <Text>{item.value}</Text>
+      </View>
+    </TouchableHighlight>
   );
+}
+
+{
+  /* <TouchableOpacity activeOpacity={0.8} onPress={() => onDelete(item.uid)}>
+      <View style={styles.list}>
+        <Text>{item.value}</Text>
+      </View>
+    </TouchableOpacity> */
 }
 
 const styles = StyleSheet.create({
